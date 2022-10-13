452ba66889f31e6ec1a0dfa9efdedb644d5acb67
Irina <i.kim1994@mail.ru>
2022-10-13 21:48:17 +0300

    решение задачи 8

---
+++ Task8/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N
+Console.Clear();
+Console.WriteLine("Введите положительное число: ");
+int numberA = int.Parse (Console.ReadLine());
+int numberB = 2;
+while(numberB <= numberA)
+{
+    Console.Write($" { numberB } ");
+    numberB = numberB + 2;
+}
