00323e9f4bab03dcf3fdf1adc377ad3944605b37
Irina <i.kim1994@mail.ru>
2022-10-19 20:11:58 +0300

    Написала решение задачи 4

--- Homework_4/Program.cs
+++ Homework_4/Program.cs
@@ -12,5 +12,5 @@ double Za = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Введите Zb. Если ищите координаты на плоскости Zb = 0: ");
 double Zb = Convert.ToDouble(Console.ReadLine());
 double dis = 0;
-dis = Math.Sqrt(Math.Pow(Xb-Xa,2)+ Math.Pow(Yb-Ya,2));
+dis = Math.Sqrt(Math.Pow(Xb-Xa,2)+ Math.Pow(Yb-Ya,2)+ Math.Pow(Zb-Za,2));
 Console.WriteLine($"расстояние:  {dis}");
