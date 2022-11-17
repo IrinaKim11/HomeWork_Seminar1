463715901dcd3de4f50f87742bfbf80e0a3cdc32
Irina <i.kim1994@mail.ru>
2022-11-17 13:36:19 +0300

    решение задачи 43

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -2,31 +2,33 @@
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
 
-// Console.WriteLine("Введите b1: ");
-// double b1 = Convert.ToDouble (Console.ReadLine() ?? "0");
-// Console.WriteLine("Введите k1: ");
-// double k1 = Convert.ToDouble (Console.ReadLine() ?? "0");
-// Console.WriteLine("Введите b2: ");
-// double b2 = Convert.ToDouble (Console.ReadLine() ?? "0");
-// Console.WriteLine("Введите k2: ");
-// double k2 = Convert.ToDouble (Console.ReadLine() ?? "0");
-
-// double x = (b2 - b1) / (k1 - k2);
-// double y = k1 * x + b1;
-
-// Console.WriteLine($"Пересечение в точке: ({x};{y})");
-
 void GetPoint(double b1, double k1, double b2, double k2)
 {
     double x = 0;
     double y = 0;
+    if(k1==k2 && b1==b2)
+    {
+        Console.WriteLine("Прямые совпадают");
+    }
+    else if(k1==k2)
+    {
+        Console.WriteLine("Прямые параллельны");
+    }
+    else
+    {
     x = (b2 - b1) / (k1 - k2);
     y = k1 * x + b1;
     Console.WriteLine($"Пересечение в точке: ({x};{y})");
+    }
 }
 
-
-GetPoint(2, 5, 4, 9);
+Console.Write("Введите числа через пробел b1 k1 b2 k2:  ");
+double[] arr = Array.ConvertAll(Console.ReadLine().Split(), double.Parse);
+double b1 = arr[0];
+double k1 = arr[1];
+double b2 = arr[2];
+double k2 = arr[3];
+GetPoint(b1, k1, b2, k2);
 
 
 
