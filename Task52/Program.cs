83b778cb2530881a7a80020138fb837a73b6ef86
Irina <i.kim1994@mail.ru>
2022-11-18 18:28:28 +0300

    решение задачи 52

---
+++ Task52/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Write("Введите количество строк массива ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива ");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+Console.WriteLine();
+GetAverage(rows, columns);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inarray)
+{
+    for (int i = 0; i < inarray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inarray.GetLength(1); j++)
+        {
+            Console.Write($"{inarray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void GetAverage(int rows, int columns)
+{
+    double average = 0;
+
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            
+            average = (average + array[i, j]);
+        }
+        average = average/rows;
+
+        Console.Write(average + "; ");
+    }
+    
+}
