75fc016387f8d539b124fb06df670caaa52f4706
Irina <i.kim1994@mail.ru>
2022-11-29 19:19:17 +0300

    решение задания 64

---
+++ Task64/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задайте значение N. Напишите программу, которая выведет все чётные числа в промежутке от N до 1.
+
+
+Console.Clear();
+
+void PrintNumbers(int min, int max)
+{
+    if (min == max && max % 2 == 0)
+    {
+        Console.Write("{0, 4}", max);
+        return;
+    }
+    else if (max % 2 == 0)
+    {
+        Console.Write("{0, 4}", max);
+    }
+    PrintNumbers(min, max - 1);
+}
+
+Console.WriteLine("Введите число: ");
+int max = int.Parse(Console.ReadLine()!);
+PrintNumbers(2, max);
